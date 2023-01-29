0a44e660428d5b0ebc8f25ab8bd2f2582da89087
Brian <brianx340@gmail.com>
2023-01-29 20:01:40 -0300

    .

---
+++ odoo_addons/school/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'School',
+    'version': '13.0.0.0.1',
+    'summary': 'Record Employee Information',
+    'description': '',
+    'category': 'Tools',
+    'author': 'Brian Rodriguez',
+    'maintainer': 'Brian Rodriguez',
+    'company': 'DevX340',
+    'website': 'https://www.linkedin.com/BrianX340',
+    'license': 'AGPL-3',
+    'depends': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/employees.xml',
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'images': ['static/description/icon.png'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
